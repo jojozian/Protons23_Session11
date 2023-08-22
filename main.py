8753b8519f4f4002efc19a4eb0a4a121f11e3ac4
jojozian <jowiriazian@gmail.com>
2023-08-22 04:07:18 +0300

    cleaning files

---
+++ main.py
@@ -0,0 +1,18 @@
+def mul(x,y):
+    print(x*y)
+    print("this num multibly\n")
+
+def sub(a,b):
+    print(a-b)
+    print("this num subtract\n")
+
+def sum(a,b):
+    print(a+b)
+    print("Its a Sum Numbers\n")
+
+num_1=int(input("Enter Your First Number: "))
+num_2=int(input("Enter Your Second Number: "))
+
+sum(num_1,num_2)
+mul(num_1,num_2)
+sub(num_1,num_2)
